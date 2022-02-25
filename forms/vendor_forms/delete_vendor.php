3e79b3c04c63f0d6afa2e51974bfe0898bb61463
Maranda <98936529+domingum6@users.noreply.github.com>
2022-02-25 14:55:22 -0500

    delete_vendor.php

---
+++ forms/vendor_forms/delete_vendor.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit']))
+{
+	//connect to database
+	include_once('../connect_mysql.php');
+
+	//variables that hold vendor data inserted from html form
+	$vendor_name=$_POST['vendor_name'];
+	$vendor_code=$_POST['vendor_code'];
+	$VID=$_POST['VID'];
+	//query setup to delete vendor
+	$query="DELETE FROM vendors WHERE vendor_name='$vendor_name' AND vendor_code='$vendor_code AND VID='$VID'";
+	//execute query
+	$result=mysqli_query($dbconn,$query) or die("Couldn't execute query");
+	
+}
