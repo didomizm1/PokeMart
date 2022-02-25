7c72d3d3bcb58a2401e00c2602d2275b43ac004e
didomizm1 <didomizm1@newpaltz.edu>
2022-02-25 11:15:22 -0500

    Login and template updates

--- forms/login_forms/login.php
+++ forms/login_forms/login.php
@@ -9,20 +9,11 @@ if(isset($_POST['submit']))
     $query = "SELECT `username`, `password` FROM `user_login`  
     WHERE `username` = '".$_POST['username']."'";
     
-    
-    //retrieve login data
-    if($dbconn->query($query)==TRUE)
-    {
-        echo nl2br("Login data query successful\n");        
-    }
-    else
-    {
-        echo nl2br("Error: " . $query . "<br>" . $dbconn->error . "\n");
-    }
-    
+    //store results and place into associative array
     $result = mysqli_query($dbconn, $query) or die("Couldn't execute query\n");
     $row = $result->fetch_array(MYSQLI_ASSOC);
 
+    //verify password
     if($row['password'] == $_POST['password'])
     {
         echo nl2br("Login successful\n");
