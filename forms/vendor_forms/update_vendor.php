b449357655e5040be83729bb227d60a329fcfb4e
Maranda <98936529+domingum6@users.noreply.github.com>
2022-02-28 23:17:56 -0500

    update_vendor.php

---
+++ forms/vendor_forms/update_vendor.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_POST['submit']))
+{
+	//connect to database
+	include_once('../connect_mysql.php');
+
+	$vendor_name=$_POST['vendor_name'];
+	$info=$_POST['info'];
+	$update=$_POST['update'];
+
+	//setup query
+	$query= UPDATE vendors SET '$info'='$update' WHERE vendor_name='$vendor_name';
+
+	//execute query
+	$result=mysqli_query($dbconn, $query) or die("Couldn't execute query\n");
+}
+?>
