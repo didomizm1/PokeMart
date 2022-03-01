2740b18c7ed71363df5d71780e128d46275ad168
didomizm1 <didomizm1@newpaltz.edu>
2022-03-01 15:22:31 -0500

    Style sheets and Age Restriction

--- forms/registration_forms/registration.js
+++ forms/registration_forms/registration.js
@@ -4,6 +4,7 @@ function validate()
     //variables
     var password = document.registration.password.value;
     var password2 = document.registration.password2.value;
+    var date_of_birth = document.registration.date_of_birth.value;
     var state = document.registration.state.value;
     var country = document.registration.country.value;
 
@@ -11,6 +12,16 @@ function validate()
     if(password != password2)
     {
         alert("Provided passwords do not match");
+        return false;
+    }
+
+    //check if the user is at least 18 years of age
+    const currentDate = new Date();
+    currentYear = currentDate.getFullYear();
+    birthYear = date_of_birth.getFullYear();
+    if(currentYear - birthYear < 18)
+    {
+        alert("You must be 18 years or older to register");
         return false;
     }
 
@@ -23,5 +34,7 @@ function validate()
     {
         alert("State not allowed for selected country");
         return false;
-    } 
+    }
+    
+    return true;
 }
