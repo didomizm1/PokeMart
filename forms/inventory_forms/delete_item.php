1ac3ce0c6acba3dc35e797132417b3d2ef8ac28e
a-espo <esposita8@newpaltz.edu>
2022-03-03 15:20:45 -0500

    Update delete_item.php

--- forms/inventory_forms/delete_item.php
+++ forms/inventory_forms/delete_item.php
@@ -1,5 +1,5 @@
 <?php
-//detele_item.php
+//delete_item.php
 
 if(isset($_POST['submit']))
 {
@@ -7,11 +7,8 @@ if(isset($_POST['submit']))
     include_once('../connect_mysql.php');
 
     //add query
-    /*
-    $query = "INSERT INTO `inventory` (`VID`,`item_name`, `japanese_item_name`, `item_description`, `item_type`, `selling_price`, `cost`, `in_stock`, `reorder_amount`, `base_stock`)
-    VALUES ('".$_POST['VID']."','".$_POST['item_name']."','".$_POST['japanese_item_name']."','".$_POST['item_description']."','".$_POST['item_type']."'
-    ,'".$_POST['selling_price']."','".$_POST['cost']."','".$_POST['in_stock']."','".$_POST['reorder_amount']."','".$_POST['base_stock']."')";
-    */
+   //$query = "DELETE FROM inventory WHERE item_name = .$_POST['item_name]."');
+
     //execute queries
    // mysqli_query($dbconn, $query) or die("Couldn't execute login data query\n");
     
@@ -23,6 +20,114 @@ if(isset($_POST['submit']))
     {
         echo nl2br("Error: " . $query . "<br>" . $dbconn->error . "\n");
     }
-
 }
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+input.right 
+    {
+    position: absolute;
+    height:25px; 
+    width:100px;
+    right: 175px;
+    top: 275px;
+    }
+</style>
+</head>
+<body>
+<form action="delete_item.php" method="post">
+
+<h2>Delete Test</h2>
+
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Delete item</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <h2> ITEM DELETE</h2>
+    <label> * Item Name: 
+		<input type = "text" name  = "item_name" maxlength = "50" required/>
+	</label>
+    <label> * IID: 
+		<input type = "number" min="1" max="6" step="1" name  = "VID" maxlength = "1" required/>
+	</label>
+   <br>
+    <input type="submit" name = submit class = "right">
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+</script>
+</form>
+</body>
+</html>
