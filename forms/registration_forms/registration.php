4a7d7c8a259214ad57b3eb80f56851172b84ffd7
didomizm1 <didomizm1@newpaltz.edu>
2022-03-07 18:07:54 -0500

    Database v09 & Shopping Cart Creation

--- forms/registration_forms/registration.php
+++ forms/registration_forms/registration.php
@@ -22,8 +22,8 @@ if(isset($_POST['submit']))
     //get ULID in a variable
     $ULIDquery = "SELECT `ULID` FROM `user_login` WHERE `username` = '".$_POST['username']."'";
     $ULIDresult = mysqli_query($dbconn, $ULIDquery) or die("Couldn't execute login data query\n");
-    $row = $ULIDresult->fetch_array(MYSQLI_ASSOC);
-    $ULID = $row['ULID'];
+    $row1 = $ULIDresult->fetch_array(MYSQLI_ASSOC);
+    $ULID = $row1['ULID'];
 
     //prepare user profile data
     $query2 = "INSERT INTO `user_profile` (`ULID`,`user_role_type`,`first_name`, `middle_name`, `last_name`, `gender`, `date_of_birth`, `email`, `home_phone_number`, `cell_phone_number`, `street_1`, `street_2`, `city`, `state`, `zip_code`, `country`)
@@ -32,10 +32,23 @@ if(isset($_POST['submit']))
 
     mysqli_query($dbconn, $query2) or die("Couldn't execute profile data query\n");
 
+    //get UPID in a variable
+    $UPIDquery = "SELECT `UPID` FROM `user_profile` WHERE `ULID` = '$ULID'";
+    $UPIDresult = mysqli_query($dbconn, $UPIDquery) or die("Couldn't execute login data query\n");
+    $row2 = $UPIDresult->fetch_array(MYSQLI_ASSOC);
+    $UPID = $row2['UPID'];
+
     //prepare customer profile data
     $query3 = "INSERT INTO `customer_profile` (`UPID`, `card_count`, `number_of_purchases`, `total_money_spent`, `active_orders`)
-    VALUES ((SELECT `UPID` FROM `user_profile` WHERE `ULID` = '$ULID'),'0','0','0','0')";
+    VALUES ('$UPID','0','0','0','0')";
+
     mysqli_query($dbconn, $query3) or die("Couldn't execute profile data query\n");
+
+    //prepare new user's shopping cart
+    $query4 = "INSERT INTO `shopping_cart` (`CPID`, `number_of_items`, `total_price`)
+    VALUES ((SELECT `CPID` FROM `customer_profile` WHERE `UPID` = '$UPID'),'0','0')";
+
+    mysqli_query($dbconn, $query4) or die("Couldn't execute profile data query\n");
     
     //send user to the login page
     header("Location: ../login_forms/login.html");
