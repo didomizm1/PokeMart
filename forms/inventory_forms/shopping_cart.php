5866070d49138e77b195649e93141967d36f2a5e
a-espo <esposita8@newpaltz.edu>
2022-03-08 01:10:13 -0500

    random updates

--- forms/inventory_forms/shopping_cart.php
+++ forms/inventory_forms/shopping_cart.php
@@ -2,12 +2,20 @@
 
 //connect to database
 include_once('../connect_mysql.php');
+//session handling
+require_once('../session.php');
+
+//get user profile data associated with logged in user
+$query = "SELECT * FROM `user_profile` WHERE `ULID` = '".$_SESSION['ULID']."'";
+$result = mysqli_query($dbconn, $query) or die("Couldn't execute query\n");
+$row = $result->fetch_array(MYSQLI_ASSOC);
+
 
 if(isset($_POST['submit']))
 {
    
-    $query ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
-    $query2 = "SELECT * FROM shopping_cart WHERE SCID = SCID";
+    $query2 ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
+    //$query2 = "SELECT * FROM shopping_cart WHERE SCID = SCID";
     
 }
 
