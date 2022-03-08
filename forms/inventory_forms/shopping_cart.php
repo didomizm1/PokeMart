685a607ec010bc30131e3da313b70336da81a797
a-espo <esposita8@newpaltz.edu>
2022-03-08 00:23:53 -0500

    update_item

--- forms/inventory_forms/shopping_cart.php
+++ forms/inventory_forms/shopping_cart.php
@@ -5,23 +5,11 @@ include_once('../connect_mysql.php');
 
 if(isset($_POST['submit']))
 {
-    $query = "SELECT * FROM `shopping_cart`";
-    $search_result = filterTable($query);
+   
+    $query ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
+    $query2 = "SELECT * FROM shopping_cart WHERE SCID = SCID";
     
 }
- else 
- {
-    $query = "SELECT * FROM `shopping_cart`";
-    $search_result = filterTable($query);
-}
 
-// function to connect and execute the query
-function filterTable($query)
-{
-    $connect = mysqli_connect("localhost","root","","pokemart_db");
-    //$connect = include_once('connect_mysql.php');
-    $filter_Result = mysqli_query($connect, $query);
-    return $filter_Result;
-}
 
 ?>
---
+++ forms/inventory_forms/update_item.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_POST['submit']))
+{
+	//connect to database
+	include_once('../connect_mysql.php');
+
+	$IID=$_POST['IID'];
+	$info=$_POST['info'];
+	$update=$_POST['update'];
+
+	//setup query
+	$query="UPDATE inventory SET $info='$update' WHERE IID='$IID'";
+
+	//execute query
+	if($dbconn->query($query)==TRUE)
+    {
+        echo nl2br("Item updated successfully\n");
+    }
+    else
+    {
+        echo nl2br("Error: " . $query . "<br>" . $dbconn->error . "\n");
+    }
+}
+
+?>
