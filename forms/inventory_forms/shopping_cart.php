578b97d731e735b86a6f9c8f20472bd118665f10
a-espo <esposita8@newpaltz.edu>
2022-03-08 19:04:14 -0500

    shopping_cart.php queries

--- forms/inventory_forms/shopping_cart.php
+++ forms/inventory_forms/shopping_cart.php
@@ -6,16 +6,24 @@ include_once('../connect_mysql.php');
 require_once('../session.php');
 
 //get user profile data associated with logged in user
-$query = "SELECT * FROM `user_profile` WHERE `ULID` = '".$_SESSION['ULID']."'";
-$result = mysqli_query($dbconn, $query) or die("Couldn't execute query\n");
+$query1 = "SELECT UPID FROM user_profile WHERE ULID = '".$_SESSION['ULID']."'";
+$result = mysqli_query($dbconn, $query1) or die("Couldn't execute query\n");
 $row = $result->fetch_array(MYSQLI_ASSOC);
+
+$query2 = "SELECT CPID FROM customer_profile WHERE UPID = '".$row['UPID']."'";
+$result = mysqli_query($dbconn, $query2) or die("Couldn't execute query\n");
+$row = $result->fetch_array(MYSQLI_ASSOC);
+
+$query3 = "SELECT SCID FROM shopping_cart WHERE CPID = '".$row['CPID']."'";
+$result = mysqli_query($dbconn, $query3) or die("Couldn't execute query\n");
+$row = $result->fetch_array(MYSQLI_ASSOC);
+
 
 
 if(isset($_POST['submit']))
 {
    
     $query2 ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
-    //$query2 = "SELECT * FROM shopping_cart WHERE SCID = SCID";
     
 }
 
