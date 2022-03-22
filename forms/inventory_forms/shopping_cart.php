278ae8b285b3a23281b77290b7eb45c4f108c9cf
a-espo <esposita8@newpaltz.edu>
2022-03-21 21:37:32 -0400

    Update shopping_cart.php

--- forms/inventory_forms/shopping_cart.php
+++ forms/inventory_forms/shopping_cart.php
@@ -11,9 +11,55 @@ $SCID = $_SESSION['SCID'];
 if(isset($_POST['submit']))
 {
    
-    $query1 ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
+    $query ="SELECT * FROM cart_item WHERE SCID = '$SCID'";
+    $search_result = filterTable($query);
     
 }
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shopping Cart Test/title>
+       <!-- <h1>PokéMart Store!</h1>-->
+
+	    <link rel = "stylesheet" href = "shopping_cart.css">
+
+    </head>
+    <body>
+        <a href = "../home_page/index.php">
+			<img id = "logo" src = "../../img/lnt/logo.png" alt = "PokeMart" width="300"> 
+        </a>
+
+        <form action="shopping_cart.php" method="post">
+            <input type="text" name="valueToSearch" placeholder="Search"><br><br>
+            <input type="submit" name="submit" value="Filter"><br><br>
+            
+            <table>
+                <tr>
+                    <th>Product ID</th>
+                    <th>Item Name</th>
+                    <th>Japanese Item Name</th>
+                    <th>Type</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                </tr>
+
+      <!-- populate table from mysql database -->
+                <?php while($row = mysqli_fetch_array($search_result)):?>
+                <tr>
+                    <td><?php echo $row['IID'];?></td>
+                    <td><?php echo $row['item_name'];?></td>
+                    <td><?php echo $row['japanese_item_name'];?></td>
+                    <td><?php echo $row['item_type'];?></td>
+                    <td><?php echo $row['item_description'];?></td>
+                    <td><?php echo $row['selling_price'];?></td>
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
+        
+    </body>
+</html>
