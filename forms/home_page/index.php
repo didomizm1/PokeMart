f54b269687203962778ad72890a3cc1f4d871655
didomizm1 <didomizm1@newpaltz.edu>
2022-03-26 14:52:04 -0400

    Home Page Update

--- forms/home_page/index.php
+++ forms/home_page/index.php
@@ -26,56 +26,56 @@
 
 	<!Side of the page with links to various pages>
 	<div id = "sidebar">
-		<br><br>
-		<a href = "index.php" class = "link">
-			<img id = "home" src = "../../img/lnt/home.png" alt = "Home">
-		</a><br><br><br><br><br>
+		<br>
+		<a href = "index.php">
+			<img id = "home" src = "../../img/lnt/home.png" alt = "Home" class = "imageLink">
+		</a><br><br>
 
 		<!Determine if links should be displayed based upon login status and employee status>
 		<?php
 			if(!(isset($_SESSION['ULID'])))
 			{
 		?>
-				<a href = "../login_forms/login.html" class = "link">
-					<img id = "login" src = "../../img/lnt/login.png" alt = "Log In">
-				</a><br><br><br><br><br>
-				<a href = "../registration_forms/registration.html" class = "link">
-					<img id = "register" src = "../../img/lnt/signup.png" alt = "Sign Up">
-				</a><br><br><br><br><br>
+				<a href = "../login_forms/login.html">
+					<img id = "login" src = "../../img/lnt/login.png" alt = "Log In" class = "imageLink">
+				</a><br><br>
+				<a href = "../registration_forms/registration.html">
+					<img id = "register" src = "../../img/lnt/signup.png" alt = "Sign Up" class = "imageLink">
+				</a><br><br>
 		<?php
 			}
 			else
 			{	
 		?>
-				<a href = "../profile_forms/profile_page.php" class = "link">
-					<img id = "profile" src = "../../img/lnt/profile.png" alt = "Profile">
-				</a><br><br><br><br><br>
+				<a href = "../profile_forms/profile_page.php">
+					<img id = "profile" src = "../../img/lnt/profile.png" alt = "Profile" class = "imageLink">
+				</a><br><br>
 		<?php
 				if($_SESSION['user_role_type'] > 0)
 				{
 		?>
-					<a href = "../employee_forms/employee_hub.php" class = "link">
-						<img id = "employee_hub" src = "../../img/lnt/employee_hub2.png" alt = "Employee Hub">
-					</a><br><br><br><br><br>
+					<a href = "../employee_forms/employee_hub.php">
+						<img id = "employee_hub" src = "../../img/lnt/employee_hub2.png" alt = "Employee Hub" class = "imageLink">
+					</a><br><br>
 		<?php
 				}
 		?>
-				<a href = "../login_forms/logout.php" class = "link">
-					<img id = "logout" src = "../../img/lnt/logout.png" alt = "Log Out">
-				</a><br><br><br><br><br>
+				<a href = "../login_forms/logout.php">
+					<img id = "logout" src = "../../img/lnt/logout.png" alt = "Log Out" class = "imageLink">
+				</a><br><br>
 		<?php
 			}
 		?>
 
-		<a href = "../inventory_forms/customer_shopping.php" class = "link">
-			<img id = "shop" src = "../../img/lnt/shop.png" alt = "Shop">
-		</a><br><br><br><br><br>
-		<a href = "../faq_forms/faq.html" class = "link">
-			<img id = "faq" src = "../../img/lnt/faq.png" alt = "FAQ">
-		</a><br><br><br><br><br>
-		<a href = "info_page.html" class = "link">
-			<img id = "info_page" src = "../../img/lnt/about_us.png" alt = "About Us">
-		</a><br><br><br><br><br>
+		<a href = "../inventory_forms/customer_shopping.php">
+			<img id = "shop" src = "../../img/lnt/shop.png" alt = "Shop" class = "imageLink">
+		</a><br><br>
+		<a href = "../faq_forms/faq.html">
+			<img id = "faq" src = "../../img/lnt/faq.png" alt = "FAQ" class = "imageLink">
+		</a><br><br>
+		<a href = "info_page.html">
+			<img id = "info_page" src = "../../img/lnt/about_us.png" alt = "About Us" class = "imageLink">
+		</a><br><br>
 	</div>
 
 	<!Bottom of the page>
