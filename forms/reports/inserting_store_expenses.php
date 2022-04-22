5119ec68c44ef8da57d46a036733000099d56141
Maranda <domingum6@newpaltz.edu>
2022-04-21 22:45:02 -0400

    store expense fix

--- forms/reports/inserting_store_expenses.php
+++ forms/reports/inserting_store_expenses.php
@@ -22,7 +22,11 @@ if(isset($_POST['submit']))
     else
     {
     //setup query, adds the cost to the specific cell depending on month, year and type of expense
-    $query="UPDATE store_expense SET $expense+='$cost' WHERE month='$month' AND year='$year";
+    $query2="SELECT * FROM store_expense WHERE month='$month' AND year='$year' AND expense='$expense'";
+    $result2=mysqli_query($dbconn, $query2);
+    $row2 = $result2->fetch_assoc(); 
+    $total_cost=$cost + $row2['cost'];//adds new expense to previous ones
+    $query="UPDATE store_expense SET $expense='$total_cost' WHERE month='$month' AND year='$year";
 
     }
     
@@ -111,7 +115,7 @@ body{
         <option value="other_expenses">Other</option> 
       </select>
     <br>
-        Cost:<input type="number" step="0.01" min='0.01'name="cost"  required>
+        Cost:<input type="number" step="0.01" name="cost"  required>
         <br>
 			<input type="submit" name="submit" value="Submit">
       <br><br>
